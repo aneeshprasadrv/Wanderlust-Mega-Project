75c2189cfccbe5e3ac0ee5175337a6d8d055b870
Aneesh Prasad <aneesh.prasad@aspiresys.kl>
2025-07-04 15:59:56 +0530

    adding ec2 user data

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
-  key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  key_name   = "terrakey"
+  public_key = file("./rvkey.pub")
 }
 
 resource "aws_default_vpc" "default" {
@@ -11,38 +11,26 @@ resource "aws_security_group" "allow_user_to_connect" {
   name        = "allow TLS"
   description = "Allow user to connect"
   vpc_id      = aws_default_vpc.default.id
-  ingress {
-    description = "port 22 allow"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+
+
+ dynamic "ingress" {
+    for_each = var.tcp_ports
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = "tcp"
+      cidr_blocks = ["167.103.7.35/32"]
+
+    }
   }
 
   egress {
-    description = " allow all outgoing traffic "
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    description = "port 80 allow"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "port 443 allow"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "mysecurity"
   }
@@ -56,6 +44,70 @@ resource "aws_instance" "testinstance" {
   tags = {
     Name = "Automate"
   }
+
+  user_data = <<EOF
+#!/bin/bash
+sudo su -
+apt update -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+
+sudo apt-get install docker.io -y
+sleep 10
+sudo usermod -aG docker ubuntu && newgrp docker
+
+sudo apt update -y
+sudo apt install fontconfig openjdk-17-jre -y
+sleep 10
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  
+sudo apt-get update -y
+sleep 5
+sudo apt-get install jenkins -y
+sleep 10
+
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update -y
+sleep 5
+sudo apt-get install trivy -y
+sleep 10
+
+docker run -itd --name SonarQube-Server -p 9000:9000 sonarqube:lts-community
+sleep 20
+
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sleep 10
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm repo add stable https://charts.helm.sh/stable
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+kubectl create namespace prometheus
+helm install stable prometheus-community/kube-prometheus-stack -n prometheus
+sleep 20
+
+EOF
+
   root_block_device {
     volume_size = 30 
     volume_type = "gp3"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,33 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "us-west-2"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-075686beab831bb7f"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
   default     = "t2.large"
+}
+
+
+variable "tcp_ports" {
+  default = [
+    { from_port = 22, to_port = 22,  description = "SSH" },
+    { from_port =  80, to_port = 80,  description = "HTTP" },
+    { from_port = 443, to_port =443,  description = "HTTPS" },
+    { from_port = 6443, to_port = 6443, description = "" },
+    { from_port = 465, to_port = 465, description = "" },
+    { from_port  = 25, to_port = 25, description = "" },
+    { from_port  = 8080, to_port = 8080, description = "" },
+    { from_port = 3000, to_port = 10000, description = "" },
+    { from_port = 30000, to_port = 32767, description = "" },
+    { from_port = 6379, to_port = 6379, description = "" },
+
+
+
+  ]
 }
